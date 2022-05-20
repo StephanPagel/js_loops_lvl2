eb55373d91eb646fee41912cab0de0d0192fa00a
StephanPagel <stephan.pagel84@gmail.com>
2022-05-20 23:46:53 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+//Lvl 2_1
+
+// imageArray = () => {
+//     let returnArray = [];
+//     for (x = 1; x < 101; x++) {
+//         if (x < 10) {
+//             returnArray.push(`image_00${x}.jpg`);
+//         } else if (x < 100) {
+//             returnArray.push(`image_0${x}.jpg`);
+//         }
+//         else {
+//             returnArray.push(`image_${x}.jpg`);
+//         }
+//     }
+//     console.log(returnArray);
+// }
+
+// imageArray();
+
+//Lvl 2_2
+
+// let input = document.getElementById('input');
+// let output = document.getElementById('output');
+// let text = [];
+
+// let loopMe = () => {
+//     output.innerHTML = '';
+//     for (let x = 1; x <= input.value; x++) {
+//         text.push('o');
+//     }
+//     output.innerHTML = `L${text.join('')}p`;
+//     output.style.color = 'lightgreen';
+//     output.style.fontSize = '40px';
+// }
+
+
+
